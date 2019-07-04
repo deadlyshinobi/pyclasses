850d8c00f77e535ceee29a8d73e42486e15a0a96
Deadly Shinobi <52080503+deadlyshinobi@users.noreply.github.com>
2019-07-04 10:13:10 +0530

    Update MyRange.py

--- MyRange.py
+++ MyRange.py
@@ -1,1 +1,10 @@
 class Range:
+  """ A class that mimic's the built-in range class."""
+  
+  def __init__(self, start, stop = None, step = 1):
+    """Initializr a Range instance.
+    
+    Semantics is similar to built-in range class.
+    """
+    if step == 0:
+      raise ValueError('step cannot be 0')
