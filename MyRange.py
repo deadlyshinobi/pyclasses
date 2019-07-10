dd3bcc166fa28e92f8fb4cda21fa7b3c9486be6d
Deadly Shinobi <52080503+deadlyshinobi@users.noreply.github.com>
2019-07-11 01:02:22 +0530

    Done

--- MyRange.py
+++ MyRange.py
@@ -8,3 +8,26 @@ class Range:
     """
     if step == 0:
       raise ValueError('step cannot be 0')
+    
+    if stop is None:
+      start , stop = 0 , start
+
+    self._length = max(0, (stop - start + step - 1)// step)
+    self._start = start
+    self._step = step
+
+  def __len__(self):
+    """Return number of entities in the range."""
+    return self._length
+  
+
+  def __getitem__(self,k):
+    """Return entry at the index."""
+    if k < 0:
+      k +=len(self)
+    
+    if not 0 <= k <self._length:
+      raise IndexError("Index out of range")
+    
+    return self._start + k * self._step
+
